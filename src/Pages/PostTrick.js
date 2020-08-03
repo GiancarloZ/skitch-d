033419871b7cb19343b720a6e35620d52fa25bfe
giancarloz <gzabaleta48@gmail.com>
2020-08-03 11:18:41 -0400

    fix recorder environment view

--- src/Pages/PostTrick.js
+++ src/Pages/PostTrick.js
@@ -62,7 +62,8 @@ export const PostTrick = props => {
                 isOnInitially
                 countdownTime={0}
                 timeLimit={10000}
-                isFlipped
+                constraints={{ audio: true, video: { facingMode: { ideal: "environment" }}}}
+                // isFlipped
                 // replayVideoAutoplayAndLoopOff 
                 onRecordingComplete={(videoBlob, video, thumbnail) => {
                     // Do something with the video...
